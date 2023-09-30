cf187951a594286004c0a67a1c35c09cec64f9ac
Aileen Kim <aileenkim1213@icloud.com>
2023-09-30 10:06:43 -0700

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Aileen Kim on 9/30/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
