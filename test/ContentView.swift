bd70c7550a2875bea5da1fd7e9b097cda7bca3a0
Aileen Kim <aileenkim1213@icloud.com>
2023-10-22 12:54:09 -0700

    code

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -1,21 +1,76 @@
-//
-//  ContentView.swift
-//  test
-//
-//  Created by Aileen Kim on 9/30/23.
-//
-
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
+    @StateObject var loc = Location()
+    @State private var uv: UVData?
+    @State private var userInputString: String = ""
+    @State private var userInput2String: String = ""
+    @State private var uvRecommendation: String = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        GeometryReader { geo in
+            ZStack{
+                Image("wallpaper")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.all)
+            }
+            VStack {
+                TextField("Enter the latitude of your location", text: $userInputString)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .offset(y: 160)
+                Text("You entered: \(userInputString)")
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .offset(y: 150)
+                TextField("Enter the longitude of your location", text: $userInput2String)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .offset(y: 140)
+                Text("You entered: \(userInput2String)")
+                    .offset(y: 130)
+                Button("Assign values") {
+                    if let latitude = Double(userInputString), let longitude = Double(userInput2String) {
+                        loc.getCurrentWeather(latitude: CLLocationDegrees(latitude), longitude: CLLocationDegrees(longitude)) { result in
+                            switch result {
+                            case .success(let response):
+                                uv = response
+                                checkUVIndex(Int(uv?.uv ?? 0))
+                            case .failure(let error):
+                                print("Error: \(error)")
+                            }
+                        }
+                    }
+                }
+                .offset(y: 135)
+
+                if let uv = uv {
+                    Text("UV Index: \(uv.uv)")
+                        .offset(y: 140)
+                    Text("Max UV Index: \(uv.uv_max)")
+                        .offset(y: 140)
+                    Text(uvRecommendation)
+                        .offset(y: 140)
+                } else {
+                    Text("Loading...")
+                        .offset(y: 140)
+                }
+            }
         }
-        .padding()
+
+        }
+    func checkUVIndex(_ uvIndex: Int) {
+        if uvIndex <= 0 {
+            uvRecommendation = "UV is low: Sunscreen is not necessary"
+        } else if uvIndex >= 3 && uvIndex <= 5 {
+            uvRecommendation = "UV is moderate: SPF 15+ Sunscreen necessary"
+        } else if uvIndex >= 6 && uvIndex <= 7 {
+            uvRecommendation = "UV is high: SPF 30+ Sunscreen necessary"
+        } else if uvIndex >= 8 && uvIndex <= 10 {
+            uvRecommendation = "UV is very high: SPF 30+ Sunscreen necessary (Apply generously)"
+        } else {
+            uvRecommendation = "UV is extreme: SPF 50+ Sunscreen necessary (LIMIT DIRECT SUN-SKIN CONTACT <10 MINUTES)"
+        }
     }
 }
 
---
+++ test/Preview Content/Location.swift
@@ -0,0 +1,51 @@
+//
+//  Location.swift
+//  test
+//
+//  Created by Aileen Kim on 10/16/23.
+//
+
+import Foundation
+import CoreLocation
+
+class Location: ObservableObject {
+
+    func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees, completion: @escaping (Result<UVData, Error>) -> Void) {
+        guard let url = URL(string: "https://api.openuv.io/api/v1/uv?lat=\(latitude)&lng=\(longitude)") else {
+            completion(.failure(NSError(domain: "Location", code: 1, userInfo: [NSLocalizedDescriptionKey: "Missing URL"])))
+            return
+        }
+
+        var urlRequest = URLRequest(url: url)
+        urlRequest.setValue("openuv-3jqrrln6anez7-io", forHTTPHeaderField: "x-access-token")
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(NSError(domain: "Location", code: 2, userInfo: [NSLocalizedDescriptionKey: "No data received"])))
+                return
+            }
+
+            do {
+                let uvResponse = try JSONDecoder().decode(UVResponse.self, from: data)
+                completion(.success(uvResponse.result))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}
+struct UVResponse: Decodable {
+    let result: UVData
+}
+
+struct UVData: Decodable {
+    let uv: Double
+    let uv_max: Double
+}
+
